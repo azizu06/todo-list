3d43bf9ffacd2eee4019cdc1fe556260e17f70f6
azizu06 <ab725492@ucf.edu>
2025-11-29 13:02:10 -0500

    Make class module for storing all proejcts

--- src/allProjects.js
+++ src/allProjects.js
@@ -1,9 +1,25 @@
-import { projects } from "./project.js";
+import { project } from "./project.js";
 
 class allProjects {
+    constructor() {
+        this.allProjects = [];
+    }
 
+    addProject(info){
+        const newProject = new project(info.name);
+        this.allProjects.push(newProject);
+    }
+
+    removeProject(info){
+        this.allProjects = this.allProjects.filter(item => item.id !== info.id)
+    }
+
+    updateProject(target, newInfo){
+        const targetProject = this.allProjects.find(item => item.id === target.id);
+        if(targetProject) targetProject.update(newInfo);
+    }
+
+    getProjects = () => this.allProjects;
 }
-
-
 
 export { allProjects }
