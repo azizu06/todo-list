808c65f811c6114dd40a7f6179efaf72ad87942e
azizu06 <ab725492@ucf.edu>
2025-12-08 22:06:31 -0500

    Refactor AllProjects class methods for consistency and clarity in project management

--- src/logic/allProjects.js
+++ src/logic/allProjects.js
@@ -1,28 +1,47 @@
-import { project } from "../project.js";
+import { project } from "./project.js";
 
 let projects = []
 
-class allProjects {
+class AllProjects {
     addProject(name){
         const newProject = new project(name);
+        const activeProject = projects.find(item => item.active === true);
+        if(activeProject) activeProject.active = false;
+        newProject.active = true;
         projects.push(newProject);
     }
 
-    removeProject(info){
-        projects = projects.filter(item => item.id !== info.dataset.id)
+    removeProject(id){
+        projects = projects.filter(item => item.id !== id)
     }
 
-    updateProject(target, newInfo){
-        const targetProject = projects.find(item => item.id === target.dataset.id);
-        if(targetProject) targetProject.update(newInfo);
+    updateProject(newName, id){
+        const targetProject = projects.find(item => item.id === id);
+        if(targetProject) targetProject.update(newName);
     }
 
-    findProject(target) {
-        const targetProject = projects.find(item => item.id === target.dataset.id);
+    findProject(id) {
+        const targetProject = projects.find(item => item.id === id);
         return targetProject;
+    }
+
+    setActive(id) {
+        const project = projects.find(item => item.active === true);
+        if(project) project.active = false;
+        const newProject = projects.find(item => item.id === id);
+        newProject.active = true;
+        return newProject
+    }
+
+    findActive(){
+        const project = projects.find(item => item.active === true);
+        return project;
     }
 
     getProjects = () => projects;
 }
+
+projects.push(new project("Default"));
+const allProjects = new AllProjects();
 
 export { allProjects };
